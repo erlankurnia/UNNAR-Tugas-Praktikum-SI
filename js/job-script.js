7090a06789dde2bedde4d72a360e437c47924c3f
R-Lan Kurnia <33871129+krlan2789@users.noreply.github.com>
2018-05-07 00:25:31 +0700

    Fix Append . .

--- js/job-script.js
+++ js/job-script.js
@@ -8,8 +8,32 @@ $('#submitBtn').click(function()
 	// Mendapatkan semua input form yg ada pada halaman html
 	$('input').each(function()
 	{
-		inputFields[indexInput] = $(this).attr('id');
-		indexInput++;
+		inputLabelID = $(this).attr('id');
+		
+		// Mengecek input form telah terisi atau tidak
+		if($(this).val() != "" && $(this).val() != undefined && $(this).val() != null && $(this).val() != 0)
+		{
+			// Menambahkan komponen didalam halaman html (Untuk menampilkan kondisi setelah melakukan Submit)
+			$('div#notifSubmition').empty();
+			$('#notifSubmition').append('<div class="notifSubmition col-sm-12"><h4 class="text-center">Data berhasil disimpan.</h4></div>');
+			$('div.notifSubmition').addClass('notifSubmition notifSuccess');
+
+			return false;
+		}
+		else
+		{
+			$('label.labelInput').each(function()
+			{
+				if($(this).text() == inputLabelID)
+				{
+					$('div#notifSubmition').empty();
+					$('#notifSubmition').append('<div class="notifSubmition col-sm-12"><h4 class="text-center">' + $(this).text() + ' tidak boleh kosong.</h4></div>');
+					$('div.notifSubmition').addClass('notifSubmition notifFailed');
+
+					return false;
+				}
+			});
+		}
 	});
 
 	// Mencetak pada console Browser
@@ -20,34 +44,5 @@ $('#submitBtn').click(function()
 		var printInputFields = "#" + inputFields[num];
 		console.log(inputFields[num] + " dengan label " + labelForId[num] + " : " + $(inputFields[num]).val());
 	}
-	/* Mencetak pada console Browser */
-
-	for(var indexNum = 0; indexNum < inputFields.length; indexNum++)
-	{
-		// Mengecek input form telah terisi atau tidak
-		if($(inputFields[indexNum]).val() != "" || $(inputFields[indexNum]).val() != undefined || $(inputFields[indexNum]).val() != null || $(inputFields[indexNum]).val() != 0)
-		{
-			if (indexNum == inputFields.length - 1)
-			{
-				// Menambahkan komponen didalam halaman html (Untuk menampilkan kondisi setelah melakukan Submit)
-				$('div#notifSubmition').empty();
-				$('#notifSubmition').append('<div class="notifSubmition col-sm-12"><h4 class="text-center">Data berhasil disimpan.</h4></div>');
-				$('div.notifSubmition').addClass('notifSubmition notifSuccess');
-
-				return false;
-			}
-			else
-			{
-				continue;
-			}
-		}
-		else
-		{
-			$('div#notifSubmition').empty();
-			$('#notifSubmition').append('<div class="notifSubmition col-sm-12"><h4 class="text-center">' + labelForId[indexNum] + ' tidak boleh kosong.</h4></div>');
-			$('div.notifSubmition').addClass('notifSubmition notifFailed');
-
-			return false;
-		}
-	}
+	// Mencetak pada console Browser 
 });
